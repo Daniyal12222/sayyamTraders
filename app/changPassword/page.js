f7fe3fc88be9be0a01ed70e680849a49118a55ec
daniyal <daniyalabbasi12222@gmail.com>
2025-02-13 22:38:42 +0500

    changpassword

---
+++ app/changPassword/page.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import { HiArrowLeft } from "react-icons/hi";
+
+export default function Login() {
+  return (
+    <div className="w-full h-[100vh] flex justify-center items-center bg-g\ ">
+      <div className="w-[40%] h-[50vh] shadow-lg   flex flex-col justify-around items-center relative">
+ <button className=" absolute left-8 top-5 "><HiArrowLeft /></button>
+        <p className="text-black font-bold text-2xl">Change Password</p>
+
+        <form  className="flex flex-col justify-center items-center gap-3 mb-2  w-[80%] " >
+          <input type="password" placeholder="current Password"  className="w-[70%] border  px-4 py-1 focus:outline-none focus:border-yellow-400 border-s-2 border-s-yellow-400 " required   />
+          <input type="password" placeholder="New Password" className="w-[70%] border px-4 py-1  focus:outline-none focus:border-yellow-400 border-s-2 border-s-yellow-400  " required  />
+          <input type="password" placeholder="Repeat Password" className="w-[70%] border px-4 py-1  focus:outline-none focus:border-yellow-400 border-s-2 border-s-yellow-400  " required  />
+          <button type="submit" className="w-[70%] px-4 py-3 text-sm text-black bg-s-orange-300 rounded hover:bg-s-yellow-400 font-semibold bg-yellow-400 mt-3">Save changes</button>
+          
+        </form>
+      </div>
+    </div>
+  );
+}
--- app/setting/page.js
+++ app/setting/page.js
@@ -7,14 +7,14 @@ export default function Settings() {
     return (
         <div className="w-full h-[100vh] flex justify-center items-center  ">
             <div className="w-[40%] h-[50vh] shadow-lg  flex flex-col justify-evenly items-center relative border">
-                <button  className=" absolute left-8 top-9 "><HiArrowLeft /></button>
+                <button className=" absolute left-8 top-9 "><HiArrowLeft /></button>
                 <div className="rounded-full bg-gray-300 absolute bottom-56">
                     <Image
-                    src={require("./../../public/people.png")}
-                    alt="people"
-                    width={100} 
-                    height={100} 
-                    className=""
+                        src={require("./../../public/people.png")}
+                        alt="people"
+                        width={100}
+                        height={100}
+                        className=""
                     />
                 </div>
                 <div>
@@ -22,10 +22,10 @@ export default function Settings() {
                     <p className="text-center text-sm text-neutral-500">daniyal@gmail.com</p>
                 </div>
 
-        <button className="w-[60%] px-4 py-3 text-sm text-black bg-s-orange-300 rounded hover:bg-s-yellow-400 font-semibold bg-yellow-400 ">Change password</button>
+                <button className="w-[60%] px-4 py-3 text-sm text-black bg-s-orange-300 rounded hover:bg-s-yellow-400 font-semibold bg-yellow-400 ">Change password</button>
 
-                
-                </div>
+
+            </div>
         </div>
     );
 }
